e561d1072cd7b8023368c1dfd89aa16d1dada020
mancinidsb <guilhermemancinidsb@gmail.com>
2024-01-04 15:07:52 -0300

    Cria o método público 'CriaCnpj' na classe 'Auxilia' e importa a pasta 'Entities' para a classe 'Program'

--- ProjetoEstoque/Entities/Auxilia.cs
+++ ProjetoEstoque/Entities/Auxilia.cs
@@ -5,5 +5,35 @@ namespace ProjetoEstoque.Entities
     internal static class Auxilia
     {
         //Classe Que Auxilia Na Classe Program
+        public static string? CriaCnpj()
+        {
+            int[] arrayNumbers = new int[10];
+            Random random = new Random();
+
+            int number;
+            for(int i = 0; i < arrayNumbers.Length; i++)
+            {
+                number = random.Next(0, 10);
+                arrayNumbers[i] = number;
+            }
+
+            string? cnpj = "";
+            for(int i=0;i<arrayNumbers.Length;i++)
+            {
+                switch (i)
+                {
+                    case 1 or 4:
+                        cnpj += $"{arrayNumbers[i]}.";
+                        break;
+                    case 7:
+                        cnpj += $"{arrayNumbers[i]}/0001-";
+                        break;
+                    default:
+                        cnpj += $"{arrayNumbers[i]}";
+                        break;
+                }
+            }
+            return cnpj;
+        }
     }
 }
--- ProjetoEstoque/Program.cs
+++ ProjetoEstoque/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ProjetoEstoque.Entities;
 
 namespace ProjetoEstoque
 {
