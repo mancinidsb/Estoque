f8d2f0dc9e6b2d97c58d1f4dfbce1958e603ab9f
mancinidsb <guilhermemancinidsb@gmail.com>
2024-01-04 15:32:00 -0300

    Cria um construtor novo a 'Produto' e altera a visibilidade da propriedade set do atributo 'Descricao' também de 'Produto'

--- ProjetoEstoque/Entities/Produto.cs
+++ ProjetoEstoque/Entities/Produto.cs
@@ -8,13 +8,21 @@ namespace ProjetoEstoque.Entities
         public string? Nome { get; private set; }
         public int Quantidade { get; private set; }
         public Fornecedor? Fornecedor { get; private set; }
-        public string? Descricao { get; private set; }
+        public string? Descricao { get; set; }
 
         public Produto(string? nome, int qtd, Fornecedor? forn)
         {
             this.Nome = nome;
             this.Quantidade = qtd;
             this.Fornecedor = forn;
+        }
+
+        public Produto(string? nome, int qtd, Fornecedor? forn, string desc)
+        {
+            this.Nome = nome;
+            this.Quantidade = qtd;
+            this.Fornecedor = forn;
+            this.Descricao = desc;
         }
     }
 }
