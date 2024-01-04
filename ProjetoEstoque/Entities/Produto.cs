88337f361f9c6edb27a2549cfbfc0501fd445cd5
mancinidsb <guilhermemancinidsb@gmail.com>
2024-01-04 15:27:03 -0300

    Cria atributos públicos 'Nome'(string), 'Descricao'(string), 'Fornecedor'(Fornecedor) e 'Quantidade'(int). E cria um construtor básico com nome, quantidade e fornecedor

--- ProjetoEstoque/Entities/Produto.cs
+++ ProjetoEstoque/Entities/Produto.cs
@@ -5,5 +5,16 @@ namespace ProjetoEstoque.Entities
     internal class Produto
     {
         //Classe Produto
+        public string? Nome { get; private set; }
+        public int Quantidade { get; private set; }
+        public Fornecedor? Fornecedor { get; private set; }
+        public string? Descricao { get; private set; }
+
+        public Produto(string? nome, int qtd, Fornecedor? forn)
+        {
+            this.Nome = nome;
+            this.Quantidade = qtd;
+            this.Fornecedor = forn;
+        }
     }
 }
