f73067637674b2437e2947e254191c24855ee4a1
mancinidsb <guilhermemancinidsb@gmail.com>
2024-01-04 17:08:54 -0300

    Testa a classe 'Fornecedor', 'Produto', 'Auxilia' e 'Estoque' no 'Program'

--- ProjetoEstoque/Program.cs
+++ ProjetoEstoque/Program.cs
@@ -8,6 +8,12 @@ namespace ProjetoEstoque
         static void Main(string[] args)
         {
             //Inicio Do Programa
+            Fornecedor forn = new("Coca-Cola", Auxilia.CriaCnpj());
+            Produto prod = new("Crystal", 5, forn, 5.0);
+            Estoque estoque = new();
+
+            estoque.InserirProduto(prod);
+            Console.WriteLine(estoque.MostraProduto("Crystal"));
         }
     }
 }
