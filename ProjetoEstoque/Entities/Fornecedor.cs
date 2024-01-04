408e96b82bd859fb80314c5202e9416ea7b5335e
mancinidsb <guilhermemancinidsb@gmail.com>
2024-01-04 14:29:20 -0300

    Cria atributos públicos 'Nome'(string) e 'Cnpj'(string) para a classe 'Fornecedor', e um construtor básico

--- ProjetoEstoque/Entities/Fornecedor.cs
+++ ProjetoEstoque/Entities/Fornecedor.cs
@@ -5,5 +5,13 @@ namespace ProjetoEstoque.Entities
     internal class Fornecedor
     {
         //Classe Fornecedor
+        public string? Nome { get; private set; }
+        public string? Cnpj { get; private set; }
+
+        public Fornecedor(string? nome, string? cnpj) 
+        {
+            this.Nome = nome;
+            this.Cnpj = cnpj;
+        }
     }
 }
