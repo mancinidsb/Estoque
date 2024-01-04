619461c38922fdc5195fbda8e584fae4a161ae7c
mancinidsb <guilhermemancinidsb@gmail.com>
2024-01-04 16:39:38 -0300

    Cria uma lista de 'Produto' de nome 'Produtos' e acrescenta o método 'InserirProduto' e 'PesquisaProduto' em 'Estoque'

--- ProjetoEstoque/Entities/Estoque.cs
+++ ProjetoEstoque/Entities/Estoque.cs
@@ -1,9 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProjetoEstoque.Entities
 {
     internal class Estoque
     {
         //Classe Estoque
+        public List<Produto> Produtos = new List<Produto>();
+
+        public int InserirProduto(Produto prod) 
+        {
+            if (this.PesquisaProduto(prod.Nome) == null)
+            {
+                this.Produtos.Add(prod);
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public Produto? PesquisaProduto(string? nome)
+        {
+            foreach(Produto prod in this.Produtos)
+            {
+                if(prod.Nome == nome)
+                {
+                    return prod;
+                }
+            }
+            return null;
+        }
     }
 }
